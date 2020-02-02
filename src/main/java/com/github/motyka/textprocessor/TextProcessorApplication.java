6b7d8b62ea6485890af7fd06a59644b0d29da8f6
Mike <motyka.mm@gmail.com>
2020-02-02 23:00:36 +0100

    initial commit

---
+++ src/main/java/com/github/motyka/textprocessor/TextProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.github.motyka.textprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextProcessorApplication.class, args);
+	}
+
+}
